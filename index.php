5056f1b3495ad98aaaf6bdef8dcfe319553810c1
Sanja Rancic <sanjarancic0@gmail.com>
2020-10-31 18:38:52 +0100

    Add search for books table

--- index.php
+++ index.php
@@ -13,50 +13,7 @@
 </head>
 
 <body>
-
-    <?php include 'obrada.php' ?>
-
-    <?php
-    if (isset($_GET['order_by'])) {
-        $order_by = $_GET['order_by'];
-    } else {
-        $order_by = 'name';
-    }
-
-    if (isset($_GET['order_dir'])) {
-        $order_dir = $_GET['order_dir'];
-    } else {
-        $order_dir = '';
-    }
-
-    $active_sort_class = "class='sort {$order_dir}'";
-    ?>
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th onclick="changeSort('id')" <?php if ($order_by == 'id') echo $active_sort_class ?>>#</th>
-                <th onclick="changeSort('name')" <?php if ($order_by == 'name')  echo $active_sort_class ?>>Name</th>
-                <th onclick="changeSort('authors')" <?php if ($order_by == 'authors')  echo $active_sort_class ?>>Authors</th>
-                <th onclick="changeSort('date_published')" <?php if ($order_by == 'date_published') echo $active_sort_class ?>>Date Published</th>
-                <th onclick="changeSort('genre')" <?php if ($order_by == 'genre') echo $active_sort_class ?>>Genre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $db->ExecuteQuery("SELECT books.*, genre.name as genre FROM books LEFT JOIN genre ON books.genre_id=genre.id ORDER BY {$order_by} {$order_dir}");
-            while ($red = $db->getResult()->fetch_object()) :
-            ?>
-                <tr>
-                    <td><?php echo $red->id; ?></td>
-                    <td><?php echo $red->name; ?></td>
-                    <td><?php echo $red->authors; ?></td>
-                    <td><?php echo $red->date_published; ?></td>
-                    <td><?php echo $red->genre ? $red->genre : '--'; ?></td>
-                </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
+    <?php include 'books_table.php' ?>
 </body>
 
 </html>
