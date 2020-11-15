a8c75e45cb051af3a38103f25767e476998e333a
Sanja Rancic <sanjarancic0@gmail.com>
2020-11-15 14:33:03 +0100

    CSS fixes

--- genre/add_genre_modal.php
+++ genre/add_genre_modal.php
@@ -1,5 +1,9 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addGenreModal">
+<button type="button" class="btn btn-link" data-toggle="modal" data-target="#addGenreModal">
+    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+        <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+    </svg>
     Add new Genre
 </button>
 
--- header.php
+++ header.php
@@ -1,11 +1,12 @@
+<?php $current_route = explode('/', $_SERVER['REQUEST_URI'])[2] ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="/domaci1/books">Books</a>
+                <a class="nav-link <?php if ($current_route == 'books') echo 'active' ?>" href="/domaci1/books">Books</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/domaci1/genre">Genres</a>
+                <a class="nav-link <?php if ($current_route == 'genre') echo 'active' ?>" href="/domaci1/genre">Genres</a>
             </li>
         </ul>
     </div>
